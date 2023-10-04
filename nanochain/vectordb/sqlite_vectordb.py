ef0ddae8b143e3d06b5ca40c4db9668d149f7919
James Liu <james.ch.liu@gmail.com>
2023-10-03 18:56:16 -0700

    Based on the code you provided, here is the commit message for the changes made:
    "Added unit tests for SQLiteVectorDB and refactored the code for better readability and maintainability."
    
    The changes include:
    
    * Adding unit tests for the SQLiteVectorDB class using pytest
    * Refactoring the code for better readability and maintainability
    * Adding a fixture to create a temporary database for each test
    * Adding tests for vector storage and search, vector update, and vector deletion
    * Using the `pytest.fixture` to create a temporary database for each test
    * Using the `os.remove` method to remove the temporary database after each test
    
    Note that the commit message is a brief summary of the changes made, and it's not a comprehensive list of all the changes.

---
+++ nanochain/vectordb/sqlite_vectordb.py
@@ -0,0 +1,78 @@
+import json
+import numpy as np
+import sqlite_utils
+from typing import List, Tuple, Optional, Dict
+from .base_vectordb import VectorDatabase
+from annoy import AnnoyIndex
+
+class SQLiteVectorDB(VectorDatabase):
+
+    def __init__(self, dimension: int, db_path=None, metric: str = "euclidean"):
+        self.dimension = dimension
+        if db_path:
+            self.db = sqlite_utils.Database(db_path)
+        self.db = self.db or sqlite_utils.Database(memory=True)
+        self.db["vectors"].create({
+            "id": int,
+            "vector": bytes,
+            "metadata": str
+        }, pk="id", if_not_exists=True)
+        
+        self.index = AnnoyIndex(dimension, metric)
+        self.index_built = False
+
+    def store_vectors(self, vectors: List[List[float]], metadata: List[dict]) -> None:
+        for vector, meta in zip(vectors, metadata):
+            numpy_array = np.array(vector, dtype=np.float32)
+            blob = numpy_array.tobytes()
+            metadata_string = json.dumps(meta)
+            row = self.db["vectors"].insert({"vector": blob, "metadata": metadata_string})
+            idx = row.last_pk
+            self.index.add_item(idx, vector)
+        self.vector_updated = True
+
+    def refresh_index(self):
+        # Drop the current index and create a new one
+        self.index = AnnoyIndex(self.dimension, "euclidean")
+        
+        # Retrieve all vectors from the SQLite database and add them to the Annoy index
+        for row in self.db["vectors"].rows_where():
+            numpy_array = np.frombuffer(row["vector"], dtype=np.float32)
+            self.index.add_item(row["id"], numpy_array.tolist())
+        
+        # Build the Annoy index
+        self.index.build(10)
+        self.vectors_updated = False
+    
+
+    def search_vectors(self, query_vector: List[float], top_k: int) -> List[Tuple[int, float, dict]]:
+        if self.vector_updated:
+            self.refresh_index()
+
+        indices, distances = self.index.get_nns_by_vector(query_vector, top_k, include_distances=True)
+        
+        results = []
+        for idx, distance in zip(indices, distances):
+            row = self.db["vectors"].get(idx)
+            metadata = json.loads(row["metadata"])
+            results.append((idx, distance, metadata))
+            
+        return results
+
+    def update_vector(self, index: int, new_vector: List[float], new_metadata: Optional[dict] = None) -> None:
+        # Convert vector to BLOB
+        numpy_array = np.array(new_vector, dtype=np.float32)
+        blob = numpy_array.tobytes()
+        # Update in SQLite
+        self.db["vectors"].update(index, {"vector": blob, "metadata": json.dumps(new_metadata or {})})
+        
+        # Update in Annoy
+        self.index.add_item(index, new_vector)
+        self.index_built = False
+
+    def delete_vector(self, index: int) -> None:
+        self.db["vectors"].delete_where("id = ?", [index])
+        self.vector_updated = True
+        # Note: Deletion in Annoy isn't straightforward. One way is to rebuild the index excluding the deleted vector.
+        # However, for simplicity, we're not handling it here. The deleted vector may still appear in search results.
+
---
+++ tests/vectordb/test_sqlite_vectordb.py
@@ -0,0 +1,67 @@
+import pytest
+from nanochain.vectordb.sqlite_vectordb import SQLiteVectorDB
+import os
+
+DB_PATH = "tests/resources/test_sql_vector.db"
+
+#@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
+def db():
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    database = SQLiteVectorDB(dimension=2, db_path=DB_PATH)
+    #yield database
+    #os.remove(DB_PATH)
+    return database
+
+def test_vector_storage_and_search(db:SQLiteVectorDB):
+    # Storing a single vector
+    vector = [1.0, 2.0]
+    metadata = {"info": "sample_vector"}
+    db.store_vectors([vector], [metadata])
+
+    # Searching the vector itself should return the same vector as the most similar one
+    results = db.search_vectors(vector, top_k=1)
+    assert len(results) == 1
+    _, _, meta = results[0]
+    assert meta["info"] == "sample_vector"
+
+def test_vector_update(db:SQLiteVectorDB):
+    # Storing a single vector
+    vector = [1.0, 2.0]
+    metadata = {"info": "sample_vector"}
+    db.store_vectors([vector], [metadata])
+
+    idx, _, _ = db.search_vectors(vector, top_k=1)[0]
+
+    # Update the vector
+    updated_vector = [2.0, 3.0]
+    updated_metadata = {"info": "updated_vector"}
+    db.update_vector(idx, updated_vector, updated_metadata)
+
+    # Searching the updated vector should now return the updated vector
+    results = db.search_vectors(updated_vector, top_k=1)
+    assert len(results) == 1
+    _, _, meta = results[0]
+    assert meta["info"] == "updated_vector"
+
+def test_vector_deletion():
+    db = SQLiteVectorDB(dimension=2, db_path=DB_PATH)
+    # Storing two vectors
+    vectors = [[1.0, 2.0], [2.0, 3.0]]
+    metadata_list = [{"info": "first_vector"}, {"info": "second_vector"}]
+    db.store_vectors(vectors, metadata_list)
+
+    idx0, disntace0, meta0 = db.search_vectors(vectors[0], top_k=1)[0]
+    # Delete the first vector
+    db.delete_vector(idx0)
+
+    # Searching the deleted vector should now return the second vector
+    results = db.search_vectors(vectors[0], top_k=1)
+    assert len(results) == 1
+    _, _, meta = results[0]
+    assert meta["info"] == "second_vector"
+
+#def teardown_module():
+def teardown_function():
+    os.remove(DB_PATH)
